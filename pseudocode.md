f64ad8f9b6bdcd4326d0d781dd8cac87352d5e62
krzemian <1569895+krzemian@users.noreply.github.com>
2024-02-06 13:17:59 +0100

    feat: Add UI calculation logic (console-only results)

--- pseudocode.md
+++ pseudocode.md
@@ -1,26 +1,4 @@
-// PSEUDOCODE //
-// When user hits a number
-    // If leftOperand is empty -> store in leftOperand
-    // If leftOperand is declared, but there's no operator -> store in leftOperand (replace it)
-    // If leftOperand is declared & operator s declared -> store in rightOperand (this replaces the current rOp if needed)
-
-// When user hits "="
-    // if two operands and an operand are declared
-        // Calculate:
-            // Find the dedicated arrow function (based on the operand)
-            // Once calculated, store value in leftOperand, #CLEAR clear operator & rightOperand
-            // TODO: Implement logic for multiple = (that would require applying the same operator & rightOperand multiple times, so I would not be clearing it in #CLEAR above)
-    // else -> do nothing
-
-// When user hits an operator button
-    // check how many numbers do we have stored in the memory
-        // if none -> do nothing (don't even register the operator)
-        // #ONE if one -> register the operator (replace the current one, if needed), but don't compute 
-        // if two -> 
-            // act as "="
-            // move the new sum to leftOperand, empty rightOperand
-            // [this should then proceed to #ONE above]
-                // replace the operator with the new one (same as #ONE)
+// PSEUDOCODE (remaining stuff) //
 
 // When user hits the CLEAR button
     // Delete lOp, rOp, operator
--- script.js
+++ script.js
@@ -14,15 +14,67 @@ const operations = {
     // TODO: Add more operations
 };
 
-// Simulate user input
-const leftOperandDOMInput = '3';
-const rightOperandDOMInput = '8';
-const operatorDOMInput = '+'; // Q: Will it be passed as string?
+document.addEventListener('DOMContentLoaded', () => {
+    const calculator = document.querySelector('.calculator');
 
-const leftOperand = +leftOperandDOMInput;
-const rightOperand = +rightOperandDOMInput;
-const operator = operatorDOMInput;
+    let leftOperand = null;
+    let rightOperand = null;
+    let operator = null;
 
-console.log(operations[operator](leftOperand, rightOperand)); // 11
-console.log(operations['*'](3, 4)); // 12
-console.log(operations['/'](20, 3)); // 6.66
+    // Catch button events
+    calculator.addEventListener('click', (click) => {
+        if (click.target.classList.contains('calculator__button--operand')) {
+            // OPERAND CLICKED
+            const operandValue = +click.target.textContent;
+
+            // If leftOperand is empty -> set it
+            if (leftOperand === null) {
+                leftOperand = operandValue;
+            } else if (typeof leftOperand === 'number' && operator === null) {
+                // If there's no operator yet -> append digits to lOp
+                // TODO: Come up with a more elegant solution
+                leftOperand = +`${leftOperand}${operandValue}`;
+            } else if (rightOperand === null) {
+                rightOperand = operandValue;
+                console.log('trzy');
+            } else if (typeof rightOperand === 'number') {
+                // If lOp & the operator are already there, set/replace the rOp
+                console.log('cztery');
+                rightOperand = +`${rightOperand}${operandValue}`;
+            }        
+        } else if (click.target.classList.contains('calculator__button--operator')) {
+            // OPERATOR CLICKED
+            const operatorValue = click.target.textContent;
+
+            if (typeof leftOperand != 'number' && typeof rightOperand != 'number') {
+                // If no digits yet, do nothing
+            } else if (typeof leftOperand === 'number' && typeof rightOperand != 'number') {
+                // If there's no rightOperand yet, set/replace the operator
+                operator = operatorValue;
+            } else if (typeof leftOperand === 'number' 
+                    && typeof rightOperand === 'number'
+                    && operator != null) {
+                console.log('calculate stuff');
+                console.log(operations[operator](leftOperand, rightOperand));
+        // if two -> 
+            // act as "="
+            // move the new sum to leftOperand, empty rightOperand
+            // [this should then proceed to #ONE above]
+                // replace the operator with the new one (same as #ONE)
+
+// When user hits "="
+    // if two operands and an operand are declared
+        // Calculate:
+            // Find the dedicated arrow function (based on the operand)
+            // Once calculated, store value in leftOperand, #CLEAR clear operator & rightOperand
+            // TODO: Implement logic for multiple = (that would require applying the same operator & rightOperand multiple times, so I would not be clearing it in #CLEAR above)
+    // else -> do nothing
+
+                // TODO: Implement a slightly different logic for reg. operators vs =
+             }
+        }
+
+        // TEMP: Show variable values
+        console.table({lOperand: leftOperand, rOperand: rightOperand, operator: operator});
+    })
+});
