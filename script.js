0bd3cd42101d5f9a6e292c190103506d847666a2
krzemian <1569895+krzemian@users.noreply.github.com>
2024-02-06 19:31:39 +0100

    feat: Add basic backspace button handling
    This works for integers and likely some floats.
    I expect it to break when backspacing into "numbers-in-progress",
    i.e. 3.4 -> 3. or 0.0001 -> 0.000 etc.

--- script.js
+++ script.js
@@ -23,13 +23,22 @@ class Calculator {
 
         this.refreshDisplay = function (value = null) {
             // TODO: Allow more (i.e. 6) decimals,
-            // but round the 0.(3) type sooner
+            // but round the 0.(3) repeating/recurring decimals
+            // Q: How would I detect repeating decimals in JS?
             display.value = Math.round(+value * 10**DECIMALS) / 10**DECIMALS;
         };
 
         this.unaliveDisplay = function () {
             display.value = '☠️☠️☠️';
         };
+
+        this.trimX = function () {
+            this.x = +this.x.toString().slice(0, -1);
+        }
+        
+        this.trimY = function () {
+            this.y = +this.y.toString().slice(0, -1);
+        }
 
         this.setX = function (x, append = false) {
             if (append) this.x = +`${this.x}${x}`;
@@ -43,9 +52,11 @@ class Calculator {
         };
 
         this.setOperator = function (operator) { this.operator = operator; };
+
         this.getX = function () { return this.x; };
         this.getY = function () { return this.y; };
         this.getOperator = function () { return this.operator; };
+
         this.clearX = function () { return this.x = null; };
         this.clearY = function () { return this.y = null; };
         this.clearOperator = function () { return this.operator = null; };
@@ -71,9 +82,30 @@ document.addEventListener('DOMContentLoaded', () => {
             // Guardrails: one dot only (.contains()?), placed in the middle
             // Input handling: "." with no x appends it to "0."
             // ! I need a way to temporarily store 0.0 and 3.0000 etc.
+            // SCENARIOS: 
+            // . -> store as 0.
+            // 123.
+            // 0.3
+            // 0.0004
+            // 0.3. -> reject if value contains "." already
+            // Now that I think of it, I will actually benefit from storing 
+            // x & y as strings
+
+            // TODO: Cap the number of digits
+
+            // TODO: Add Backspace
+            // Works on y if not null, then on x if not null
+            // or is ignored. The presence of an operator doesn't matter
+            // Will empty the equalsBuffer
 
             // TODO!: Implement negative numbers! ("-" allowed as x sign, too)
             // Consider a separate +/- button?
+            // SCENARIOS:
+            // 34 * 53 -> [+/-] pressed -> calculate (here: multiply), then negate
+            // … -> [+/-] -> activate "-", display the negative prompt 
+            //    (AGAIN, IN NEED FOR STORING NUMBER-IN-PROGRESS VALUES LIKE -, 0., 3.000)
+            // 41.5 … … -> [+/-] -> just negate
+            // 13 -/+ -> [+/-] -> just negate 13, nullify the operator
 
             // If x is empty -> set it
             if (x === null) {
@@ -101,6 +133,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 calculator.clearY();
                 calculator.clearOperator();
                 calculator.refreshDisplay();
+            } else if (operatorValue === 'BCKSPC') {
+                // TODO!: It needs to handle NUMBER-IN-PROGRESS values (i.e. 0.4 -> 0.)
+                if (typeof y === 'number') {
+                    calculator.trimY();
+                    calculator.refreshDisplay(calculator.getY());
+                    // WIP: Remove one character of y
+                } else if (typeof x === 'number') {
+                    calculator.trimX();
+                    calculator.refreshDisplay(calculator.getX());
+                    calculator.clearOperator();
+                    // WIP: Remove one character of x
+                }
+
             } else if (typeof x === 'number' && typeof y != 'number' && operatorValue != '=') {
                 // If it's number + %, calculate it
                 if (operatorValue === '%') {
@@ -158,6 +203,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 //   This would require applying the same operator & y multiple times
                 //   Hence will likely require changes in 
                 //   the operand/operator/result memory storage mechanism
+                // CURRENT BEHAVIOR: operator and y are cleared after "="
+                // DESIRED BEHAVIOR: 
+                //  Keep operator (not "=") and y in place, so that multiple "=" can be triggered
+                //    QUESTION: What would happen if I then press a different operator?
+                //      I'd have x y and a previous operator AND a new (just clicked) operatorValue
+                //      Currently, this would calculate x [prevOp] y, then replace prevOp with operatorValue
+                //      But that shouldn't be happening (it should treat it as if there was no operator)
+                //          !!!SOLUTION IDEA 1: after "=", clear x and operator, but store it in a special equalBuffer (name tbd)
+                //          So that as long as the "=" is getting pressed, execute that
+                //          HOWEVER! As soon as anything other than "=" is pressed, the equalBuffer MUST be wiped clean
+                //          (so basically, whenever you perform any operation on numbers, wipe equalBuffer clean; whenever you perform
+                //          any operation on non-"=" operands, wipe equalBuffer clean). Names: multiEqualsBuffer
+                // IDEA: Make it a rule to never store "=" as an operator (use it as a special trigger)
+                // IDEA: Perhaps I could implement a stack structure and push elements onto it
              }
         }
 
